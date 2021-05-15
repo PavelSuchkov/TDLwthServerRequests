2fc22d284f677d2876979f8b0e8b50a0f4e5ebca
PavelSuchkov <suchkovp@gmail.com>
2021-05-15 12:42:57 +0300

    1st commit

---
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,33 @@
+import React, {ChangeEvent, useState} from 'react';
+import {TextField} from '@material-ui/core';
+import {RequestStatusType} from "../../app/app-reducer";
+
+type EditableSpanPropsType = {
+    value: string
+    onChange: (newValue: string) => void
+    disabled: boolean
+}
+
+export const EditableSpan = React.memo(function (props: EditableSpanPropsType) {
+
+    let [editMode, setEditMode] = useState(false);
+    let [title, setTitle] = useState(props.value);
+
+    const activateEditMode = () => {
+        if(!props.disabled){
+            setEditMode(true);
+            setTitle(props.value);
+        }
+    }
+    const activateViewMode = () => {
+        setEditMode(false);
+        props.onChange(title);
+    }
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    return editMode
+        ?  <TextField value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
+        : <span onDoubleClick={activateEditMode} >{props.value}</span>
+});
